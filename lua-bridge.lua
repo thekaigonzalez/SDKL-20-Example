1fe2dee7f49d436c479154ba2bcd363a29f4479f
Kai D. Gonzalez <gkai70264@gmail.com>
2021-08-24 19:34:32 -0400

    first commit

---
+++ lua-bridge.lua
@@ -0,0 +1,16 @@
+-- $Id: lua-bridge.lua
+-- hello SDKL!
+-- SDKL also compiles cleanly as Lua code! since it's built upon the Lua 5.4 Source Code.
+
+-- You can download the nanoRC from the official website
+
+-- thekaigonzalez.github.io/mirror/SDKL/sdkl.nanorc
+
+-- you can download the interpreter and files from 
+
+-- thekaigonzalez.github.io/mirror/SDKL/interpreter/sdkl
+
+-- and the (modified) source code from 
+
+-- thekaigonzalez.github.io/mirror/SDKL/source.tar.gz
+println("hello, SDKL!")
